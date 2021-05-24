b2f9acf708829dadd6e7e92a62efb5ac69d3c9ea
黄聪 <cong.huang02@hand-china.com>
2021-05-24 10:13:47 +0800

    [init]:重构

---
+++ src/quickSoft.ts
@@ -0,0 +1,23 @@
+function qucikSoft(arr: number[], left: number, right: number): void {
+  if (left === undefined) left = 0;
+  if (right === undefined) right = arr.length - 1;
+  if (left >= right) return;
+
+  const mid = getMid(arr, left, right);
+  qucikSoft(arr, left, mid - 1);
+  qucikSoft(arr, mid + 1, right);
+}
+
+function getMid(arr: number[], left: number, right: number): number {
+  const pivot = arr[left];
+  while (left < right) {
+    while (arr[right] >= pivot && left < right) right--;
+    arr[left] = arr[right];
+    while (arr[left] <= pivot && left < right) left++;
+    arr[right] = arr[left];
+  }
+  arr[left] = pivot;
+  return left;
+}
+
+module.exports = qucikSoft;
